2f6faecad631bc9e236702a34b8366bb903d40aa
Bert Lempens <bertlempens@gmail.com>
2019-02-25 14:13:40 +0100

    notes verwijderen

--- notes-app/main.js
+++ notes-app/main.js
@@ -1,10 +1,23 @@
 new Vue({
   el: '#app',
   data: {
-    activeNote: '',
+    activeNote: false,
   	notes: [
       { text: 'This is a first note', favorite: false },
       { text: 'This is a second note', favorite: true }
     ]
   },
+  methods: {
+    deleteNote() {
+      // enkel verwijderen als er een item geselecteerd is
+      if (this.activeNote != false) {
+        // index van active note opzoeken in notes
+        var index = this.notes.indexOf(this.activeNote);
+        // active note verwijderen uit notes
+        this.notes.splice(index, 1);
+        // active note terug leeg maken na verwijderen
+        this.activeNote = false;
+      }
+    }
+  }
 })
