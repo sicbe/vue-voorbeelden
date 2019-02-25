9bc3733e1f3aab1ad59ee4648d641c34178d9375
Bert Lempens <bertlempens@gmail.com>
2019-02-25 14:00:48 +0100

    active note

--- notes-app/main.js
+++ notes-app/main.js
@@ -1,9 +1,10 @@
 new Vue({
   el: '#app',
   data: {
+    activeNote: '',
   	notes: [
       { text: 'This is a first note', favorite: false },
-      { text: 'This is a second note', favorite: true },
+      { text: 'This is a second note', favorite: true }
     ]
   },
 })
